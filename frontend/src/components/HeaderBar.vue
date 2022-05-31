f2c67b873e7460bca306011360af21c1496c68ef
tz039e <tz039e@gmail.com>
2022-05-31 22:08:58 +0800

    refactor searchbar... & use pinia

---
+++ frontend/src/components/HeaderBar.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="header m-2 flex align-items-center">
+    <RouterLink :to="{ name: 'home' }" class="logo flex align-items-center">
+      <img class="image" src="@/assets/justice.png" />
+      <div class="text">
+        Case<br />
+        Lookup
+      </div>
+    </RouterLink>
+    <SearchBar :with-button="false" />
+  </div>
+</template>
+
+<script setup lang="ts">
+import { RouterLink } from 'vue-router';
+import SearchBar from './SearchBar.vue';
+</script>
+
+<style scoped lang="scss">
+.logo {
+  .image {
+    height: 5rem;
+    margin-right: 0.2rem;
+  }
+  .text {
+    font-size: 1.2rem;
+    font-weight: 700;
+    font-family: Verdana;
+    color: #4e4f52;
+  }
+  margin-left: 1.1rem;
+  margin-right: 3rem;
+}
+
+a:link {
+  text-decoration: none;
+}
+</style>
--- frontend/src/components/SearchBar.vue
+++ frontend/src/components/SearchBar.vue
@@ -1,36 +1,42 @@
 <template>
-  <form class="searchbar" @submit="handleSubmit">
-    <InputText type="text" v-model="searchValue" class="mr-2" />
+  <form v-if="withButton" @submit="handleSubmit">
+    <InputText type="text" v-model="search.value" class="searchbar mr-2" />
     <Button icon="pi pi-search" type="submit" />
+  </form>
+  <form v-else>
+    <span class="p-input-icon-left">
+      <i class="pi pi-search" />
+      <InputText
+        type="text"
+        v-model="search.value"
+        class="searchbar"
+        placeholder="Search"
+      />
+    </span>
   </form>
 </template>
 
 <script setup lang="ts">
-import { ref } from 'vue';
 import { useRouter } from 'vue-router';
 import InputText from 'primevue/inputtext';
 import Button from 'primevue/button';
+import { useSearchStore } from '@/stores/search';
+
+defineProps<{
+  withButton: boolean;
+}>();
 
 const router = useRouter();
-const searchValue = ref('');
+const search = useSearchStore();
 
 const handleSubmit = (e: Event) => {
   e.preventDefault();
-  if (!searchValue.value) return;
+  if (!search.value) return;
   router.push({
     name: 'search',
     params: {
-      query: searchValue.value,
+      query: search.value,
     },
   });
 };
 </script>
-
-<style scoped>
-.searchbar {
-  height: fit-content;
-}
-.p-inputtext {
-  width: 65vh;
-}
-</style>

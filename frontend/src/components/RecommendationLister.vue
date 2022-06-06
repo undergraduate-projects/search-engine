0faebe0857adc6913652d1d5b21b8b9e3d922a32
tz039e <tz039e@gmail.com>
2022-06-07 02:13:29 +0800

    recommendation api

---
+++ frontend/src/components/RecommendationLister.vue
@@ -0,0 +1,63 @@
+<template>
+  <div class="header">
+    <div class="title">{{ title }}</div>
+    <Button
+      icon="pi pi-refresh"
+      class="p-button-rounded p-button-text refresh"
+      @click="refresh"
+    />
+  </div>
+
+  <template
+    v-for="(item, i) in data.slice(page * pageSize, (page + 1) * pageSize)"
+    :key="i"
+  >
+    <div class="item">
+      {{ page * pageSize + i + 1 }}
+      <RouterLink :to="{ name: 'case', params: { id: item._id } }">
+        {{ item._source['全文']['文首']['案号'] }}
+      </RouterLink>
+    </div>
+  </template>
+</template>
+
+<script setup lang="ts">
+import { ref } from 'vue';
+import { RouterLink } from 'vue-router';
+import Button from 'primevue/button';
+
+const props = defineProps<{
+  title: string;
+  data: any[];
+}>();
+
+const page = ref(0);
+const pageSize = 5;
+
+function refresh() {
+  page.value = (page.value + 1) % Math.ceil(props.data.length / pageSize);
+}
+</script>
+
+<style scoped lang="scss">
+.header {
+  display: flex;
+  align-items: center;
+  .title {
+    font-size: 1rem;
+    font-weight: 700;
+  }
+  .refresh {
+    margin-left: auto;
+    height: 2.2rem;
+    width: 2.2rem;
+  }
+}
+a:link {
+  text-decoration: none;
+  color: var(--text-color);
+}
+.item {
+  margin: 0.5rem 0;
+}
+</style>
--- frontend/src/views/CaseView.vue
+++ frontend/src/views/CaseView.vue
@@ -1,21 +1,83 @@
 <script setup lang="ts">
 import HeaderBar from '../components/HeaderBar.vue';
 import Toast from 'primevue/toast';
+import { useToast } from 'primevue/usetoast';
 import { useSearchStore } from '@/stores/search';
-import { useRoute } from 'vue-router';
+import { onBeforeRouteUpdate, useRoute } from 'vue-router';
+import { api } from '@/router/axios';
+import { onMounted, ref } from 'vue';
+import RecommendationLister from '../components/RecommendationLister.vue';
+
+const toast = useToast();
 
 const search = useSearchStore();
 const route = useRoute();
-const item = search.result.data.find((item) => item.id === route.params.id);
 
-const casea = item ? item.source : {};
+const casea = ref<any>();
+
+const recommendTypeMapping = {
+  AY: '案由',
+  JBFY: '经办法院',
+  FGCY: '法官成员',
+  knn: '智能推荐',
+};
+const recommendation = ref({
+  data: {
+    AY: [],
+    JBFY: [],
+    FGCY: [],
+    knn: [],
+  },
+  offset: 0,
+  size: 0,
+  total: 0,
+});
+function recommend() {
+  api
+    .post('recommend', {
+      query_vector: casea.value['vec'],
+      AY: casea.value['案例属性']['案由'],
+      JBFY: casea.value['案例属性']['经办法院'],
+      FGCY: casea.value['案例属性']['法官成员'],
+    })
+    .then(({ data }) => {
+      recommendation.value = data;
+    })
+    .catch((error) => {
+      console.error(error);
+      toast.add({
+        severity: 'error',
+        summary: 'Request failed',
+        detail: error.message,
+        life: 3000,
+      });
+    });
+}
+async function load(id: string) {
+  const item = search.result.data.find((item) => item.id === id);
+  if (!item) {
+    const { data } = await api.get('search-by-id', {
+      params: {
+        id: id,
+      },
+    });
+    casea.value = data.data;
+  } else {
+    casea.value = item.source;
+  }
+  recommend();
+}
+onMounted(() => load(route.params.id as string));
+onBeforeRouteUpdate(async (to) => {
+  await load(to.params.id as string);
+});
 </script>
 
 <template>
   <div class="main flex flex-column">
     <Toast />
     <HeaderBar />
-    <div class="body">
+    <div v-if="casea" class="body">
       <div class="content">
         <h1 style="text-align: center">
           {{
@@ -57,7 +119,14 @@ const casea = item ? item.source : {};
           </template>
         </div>
       </div>
-      <div class="sidebar"></div>
+      <div class="sidebar">
+        <div class="recommend">
+          <h3>推荐案例</h3>
+          <template v-for="(v, k) in recommendation.data" :key="k">
+            <RecommendationLister :title="recommendTypeMapping[k]" :data="v" />
+          </template>
+        </div>
+      </div>
     </div>
   </div>
 </template>
@@ -73,7 +142,7 @@ const casea = item ? item.source : {};
   margin-bottom: 3rem;
 }
 .content {
-  padding: 1rem 2rem;
+  padding: 1rem 0.5rem 1rem 2rem;
   margin: 0rem 1rem;
   color: var(--text-color);
   line-height: 2;
@@ -106,5 +175,13 @@ const casea = item ? item.source : {};
   border-color: #71aaff;
   border-width: 0 0 0 5px;
   border-style: solid;
+}
+
+.recommend {
+  padding: 1rem;
+  margin: 2rem 1rem 1rem 0rem;
+  border-radius: 10px;
+  box-shadow: 0px 0px 5px #ccc;
+  color: var(--text-color);
 }
 </style>
